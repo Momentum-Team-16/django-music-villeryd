24017ba56240d5705dde8c5bbececf8da3aff299
Daniel <danielvillery@gmail.com>
2023-01-03 16:06:27 -0500

    added music model

--- music/models.py
+++ music/models.py
@@ -4,6 +4,29 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    bio = models.TextField(max_length=500, blank = True, null = True)
-    location = models.CharField(max_length=30, blank=True, null = True)
-    birth_date = models.DateField(blank=True, null = True)
+    bio = models.TextField(max_length=500, blank=True, null=True)
+    location = models.CharField(max_length=30, blank=True, null=True)
+    birth_date = models.DateField(blank=True, null=True)
+
+
+class Song(models.Model):
+    HIP_HOP = 'HH'
+    POP = 'PO'
+    ROCK = 'RO'
+    COUNTRY = 'CO'
+    ALTERNATIVE = 'ALT'
+
+    GENRE_CHOICES = [
+        (HIP_HOP, 'Hip-Hop/Rap'),
+        (POP, 'Pop'),
+        (ROCK, 'Rock'),
+        (COUNTRY, 'Country'),
+        (ALTERNATIVE, 'Alternative'),
+    ]
+
+    name = models.CharField(max_length=50)
+    album = models.CharField(max_length=200)
+    artist = models.CharField(max_length=100)
+    release_date = models.DateField(blank=True, null=True)
+    genre = models.CharField(max_length=3, choices=GENRE_CHOICES)
+    image = models.ImageField(blank=True, null=True)
--- music/views.py
+++ music/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+# this is where actions happen. they are triggered by the user (or an AJAX request) visiting a url
+
+
+def index(request):
+    context = {}
+    return render(request, 'music/index.html', context)
